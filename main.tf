e56906ce53393bfea91c9d284e8c778a19acb671
Samar Dash <12dit152@gmail.com>
2025-05-31 12:41:23 +0100

    initialize project structure with Terraform configuration and VCS settings

---
+++ main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+module "cloudwatch" {
+  source = "./cloudwatch"
+}
+
+module "iam" {
+  source = "./iam"
+}
+
+module "vpc" {
+  source = "./vpc"
+  private_subnet_flow_log_allow_arn = module.cloudwatch.private_subnet_flow_log_allow_arn
+  private_subnet_flow_log_deny_arn  = module.cloudwatch.private_subnet_flow_log_deny_arn
+  public_subnet_flow_log_allow_arn  = module.cloudwatch.public_subnet_flow_log_allow_arn
+  public_subnet_flow_log_deny_arn   = module.cloudwatch.public_subnet_flow_log_deny_arn
+  vpc_flow_logs_role_arn            = module.iam.vpc_flow_logs_role_arn
+}
+
+module "security_groups" {
+  source = "./security_groups"
+  vpc_id = module.vpc.samar_vpc_id
+}
+
---
+++ security_groups/main.tf
@@ -0,0 +1,109 @@
+variable "vpc_id" {
+  description = "The VPC ID to associate with security groups."
+  type        = string
+}
+
+resource "aws_security_group" "https_public" {
+  name        = "allow-https-traffic-from-everywhere"
+  description = "Allow https traffic from everywhere"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = { Name = "https-public" }
+}
+
+resource "aws_security_group" "ssh_public" {
+  name        = "allow-ssh-access-from-everywhere"
+  description = "Allow SSH access from everywhere"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "ssh-public"
+  }
+}
+
+resource "aws_security_group" "ssh_http_public" {
+  name        = "allow-ssh-and-http-traffic-from-everywhere"
+  description = "Allow SSH And HTTP Traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "ssh-http-public"
+  }
+}
+
+resource "aws_security_group" "http_public" {
+  name        = "allow-http-traffic-from-everywhere"
+  description = "Allow http traffic from everywhere"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "http-public"
+  }
+}
+
