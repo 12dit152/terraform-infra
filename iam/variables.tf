e3e470bf642270c3f71ee69b6fb4ffab41c12e2f
Samar Dash <12dit152@gmail.com>
2025-10-18 09:28:10 +0100

    removed old structure

--- iam/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "github_org" {
-  description = "GitHub organization or username"
-  type        = string
-  default     = "12dit152"
-}
--- route53/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "api_gateway_domain_name" {
-  description = "API Gateway regional domain name"
-  type        = string
-}
-
-variable "api_gateway_zone_id" {
-  description = "API Gateway regional zone ID"
-  type        = string
-}
